0f72dfaacd2f54bd823d41d7cd47127f4fe3a5a0
JaminK0100 <jaminkwong@gmail.com>
2025-06-18 21:46:07 +0000

    Added review and changed imports

--- main.py
+++ main.py
@@ -8,15 +8,22 @@ import numpy as np
 nInst = 50
 currentPos = np.zeros(nInst)
 
-
-def getMyPosition(prcSoFar):
+# Sample getMyPosition from SIG
+def getMyPosition_example(prcSoFar):
     global currentPos
     (nins, nt) = prcSoFar.shape
     if (nt < 2):
         return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
+    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])      
     lNorm = np.sqrt(lastRet.dot(lastRet))
     lastRet /= lNorm
     rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
     currentPos = np.array([int(x) for x in currentPos+rpos])
     return currentPos
+# Problems: the function is too simple, right now it is simply saying long on whatever went up yesterday and short on what went down
+# Need to make new strategy
+
+def getMyPosition(prcSoFar):
+    return
+
+
